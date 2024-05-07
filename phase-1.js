68160d59d9716bf7343e8143f3feb7f28a8d8796
Omega Dziko F. <megaoy@yahoo.com>
2024-05-07 19:33:30 -0400

    first algo built

--- phase-1.js
+++ phase-1.js
@@ -1,6 +1,14 @@
 // Adds up positive integers from 1-n
 function addNums(n) {
-  // Fill this in
+
+  let num = 0;
+
+  for (let i = 1; i <= n; i++) {
+
+           num += i;
+  }
+
+  return num;
 
 }
 
